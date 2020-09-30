b7f2478674f07b65c0d054382c0446199b8ce094
Thanakorn <60030041@lmitl.ac.th>
2020-09-30 15:56:25 +0700

    1

--- express.js
+++ express.js
@@ -6,8 +6,8 @@ app.get('/ping', function(req, res) {
 });
 
 app.get('/ping/:id', function(req, res) {
-    res.send({ping:'Hello world'});
+    res.send({ping:'hello this is server and I am got '+req.params.id});
 });
 
-app.listen(3010);
+app.listen(3000);
 console.log('Listening on port 3000...');
